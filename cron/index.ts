df2a59c95df8db84bc4b67dd68eddb8282ad1976
illiashkurenko <ilya-shkurenko@mail.ru>
2022-05-31 01:43:09 +0300

    add loging

--- cron/index.ts
+++ cron/index.ts
@@ -3,6 +3,7 @@ import {BinanceBot, getBinanceConfig } from "../bot";
 const cron = require('node-cron');
 let bot: any;
 let currentSymbol = 'LUNAUSDT';
+let requestsCount = 0;
 getBinanceConfig().then(async data => {
     bot = new BinanceBot(data);
     console.log(Date.now());
@@ -20,13 +21,16 @@ export const placeOrder = async (symbol: string, botInstance?: any) => {
     do {
         try {
             const { askPrice } = await bot.binance.bookTickers(symbol);
+            requestsCount++;
             price = parseFloat(askPrice);
             const quantity = (amountUSD / askPrice).toFixed(2);
             console.log(`amount - ${amountUSD}, quantity - ${quantity}`);
             if(!isNaN(parseFloat(quantity)) && parseFloat(quantity) !== Infinity) {
                 const response = await bot.binance.marketBuy(symbol, quantity);
+                requestsCount++;
                 console.log(response)
             }
+            console.log(requestsCount)
         } catch (e: any) {
             const error = JSON.parse(e.body || 'null');
             console.log(symbol);
